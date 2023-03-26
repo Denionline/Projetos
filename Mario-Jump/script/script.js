d25346016bc46c82560d4e1e5f8dfd7cdb467190
Daniel Ximenes <93453046+Denionline@users.noreply.github.com>
2023-03-26 18:31:37 -0300

    up

--- Mario-Jump/script/script.js
+++ Mario-Jump/script/script.js
@@ -1,6 +1,18 @@
-const mario = document.querySelector('.mario')
-const pipe = document.querySelector('.pipe')
+const game = document.querySelector('.game')
+const mario = document.querySelector('#mario')
+const pipe = document.querySelector('#pipe')
 const tryagain = document.querySelector('.tryAgain')
+const startScreen = document.querySelector('.startScreen')
+const pipeClass = document.querySelector('.pipe')
+const points = document.querySelector('#points')
+const score = document.querySelector('#score')
+var end = 0
+var point = 0;
+
+function play () {
+    startScreen.style.display = 'none'
+    pipeClass.style.display = 'block'
+}
 
 const jump = () => {
     mario.classList.add('jump')
@@ -11,10 +23,9 @@ const jump = () => {
 }
 
 const loop = setInterval(() => {
-
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
-
+    // console.log(point)
 
     if(pipePosition < 121 && pipePosition > 0 && marioPosition < 60){
         pipe.style.animation = 'none'
@@ -28,14 +39,19 @@ const loop = setInterval(() => {
         mario.style.marginLeft = '50px'
 
         tryagain.style.display = 'grid'
-
+        end = 1
+        // score.innerHTML = point
+    } else if(pipePosition < 121 && pipePosition > 0){
+        point = point + 1
+        points.innerHTML = point
     }
 }, 10);
 
 window.addEventListener('keydown', jump)
-window.addEventListener('click', jump)
 window.addEventListener('touch', jump)
 
 document.querySelector('#again').addEventListener('click', () => {
-    location.reload()
+    location.reload();    
 })
+
+document.querySelector('#start').addEventListener('click', play)
