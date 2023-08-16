982e83858dc7bc735b0d5697a9c45db302f02d9a
Daniel Ximenes <93453046+Denionline@users.noreply.github.com>
2023-08-15 22:42:02 -0300

    up

---
+++ byMe/Choose a movie/scripts/index.js
@@ -0,0 +1,2 @@
+const btnBegin = document.querySelector('.box_begin_btn-begin');
+btnBegin.addEventListener('click', () => window.location = 'pages/list.html')
--- byMe/Choose a movie/scripts/script.js
+++
@@ -1,6 +0,0 @@
-const btnBegin = document.querySelector('.box_begin_btn-begin');
-
-btnBegin.addEventListener('click', () => {
-    window.location = 'pages/list.html'
-})
-
