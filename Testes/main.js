c16f99451dc895f29e89951b44c957bfbdd74752
Daniel Ximenes <93453046+Denionline@users.noreply.github.com>
2023-09-14 21:50:52 -0300

    up

--- Testes/main.js
+++
@@ -1,9 +0,0 @@
-const listaTeclas = document.querySelectorAll('.tecla')
-
-for (i=0;i<listaTeclas.length;i++){
-    const tecla = listaTeclas[i]
-    const som = tecla.classList[1]
-    tecla.addEventListener('click', () => {
-        document.querySelector(`#som_${som}`).play()
-    })
-}
