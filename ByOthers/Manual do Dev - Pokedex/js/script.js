29e09572b4865e07b31b455701287b4d82802527
Daniel Ximenes <93453046+Denionline@users.noreply.github.com>
2023-09-26 22:18:26 -0300

    up

--- ByOthers/Manual do Dev - Pokedex/js/script.js
+++ ByOthers/Manual do Dev - Pokedex/js/script.js
@@ -1,3 +1,54 @@
-const fetchPokemon = (pokemon) => {
-    const APIresponse = fetch('')
+const pokemonName = document.querySelector('.pokemon_name');
+const pokemonNumber = document.querySelector('.pokemon_number');
+const pokemonImage = document.querySelector('.pokemon_image');
+
+const form = document.querySelector('.form');
+const input = document.querySelector('.input_search');
+
+const btnPrev = document.querySelector('.btn-prev');
+const btnNext = document.querySelector('.btn-next');
+
+let searchPokemon = 1;
+
+const fetchPokemon = async (pokemon) => {
+    const conexao = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    if(conexao.status == 200){
+        const conexaoTraduzida = await conexao.json();
+        return conexaoTraduzida;
+    }
 }
+
+const renderPokemon = async (pokemon) => {
+    pokemonName.innerHTML = 'Loading...';
+    pokemonNumber.innerHTML = '';
+    const data = await fetchPokemon(pokemon);
+    if(data){
+        pokemonImage.style.display = 'block';
+        pokemonName.innerHTML = data.name;
+        pokemonNumber.innerHTML = data.id + ' - ';
+        pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
+        input.value = '';
+        searchPokemon = data.id;
+    }else{
+        pokemonName.innerHTML = 'Not found :c';
+        pokemonImage.style.display = 'none';
+    }
+}
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    renderPokemon(input.value.toLowerCase());
+})
+
+btnPrev.addEventListener('click', () => {
+    if(searchPokemon > 1){
+        searchPokemon--;
+        renderPokemon(searchPokemon);
+    }
+})
+btnNext.addEventListener('click', () => {
+    searchPokemon++;
+    renderPokemon(searchPokemon);
+})
+
+renderPokemon(searchPokemon);
