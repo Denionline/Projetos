e8c1634b7ef00e72452e7ec646a54810ec1c1a58
Daniel Ximenes <93453046+Denionline@users.noreply.github.com>
2023-10-01 12:33:53 -0300

    up

---
+++ ByOthers/carrinho-de-compras/src/componentes/SearchBar/SearchBar.jsx
@@ -0,0 +1,26 @@
+import React, { useState} from 'react';
+import { BiSearch } from 'react-icons/bi';
+
+import './SearchBar.css';
+
+function SearchBar(){
+
+  const [searchValue, setSearchValue] = useState('');
+
+  return (
+    <form className="search-bar">
+      <input 
+        type="search"
+        value={searchValue}
+        placeholder="Buscar produtos" 
+        className="search__input"
+        onChange={ ({target}) => setSearchValue(target.value) }
+        required/>
+      <button type="submit" className="search__button">
+        <BiSearch />
+      </button>
+    </form>
+  );
+}
+
+export default SearchBar;
